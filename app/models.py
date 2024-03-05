780d1ba49556f59ee74b6cddc01629b572e4db22
meli122 <melika12329@gmail.com>
2024-03-05 17:57:45 -0500

    adding template for database

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from mongoengine import *
+
+# data structure for mongoDB
+class User(Document):
+    username: str
+    password: int
+
+class registerRequest(BaseModel):
+    name: str
+    lastname: str
+    codemelli: int
+    number: int
+    email: str
