a391511a1e3fe74fb07ec7d6dcb5694345438d3d
meli122 <melika12329@gmail.com>
2024-03-05 19:13:33 -0500

    api for login and register

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import sentry_sdk, uvicorn, uuid
 from mongoengine import *
 from fastapi import FastAPI
 from config import config
-import models
+from models import registerRequest, User
 
 cnf = config()
 
@@ -19,14 +19,19 @@ mongoUri = cnf.mongo_uri
 #FastAPI
 app = FastAPI()
 
-@app.get('/register')
-def create_user:
+@app.post('/register')
+def create_user(reg: registerRequest):
+    name = reg.name
+    lastname: reg.lastname
+    email: reg.email
+    return {"message": f"User {email} registered successfully"} 
     
 
-@app.get('/login')
-def login():
-    
-
+@app.post('/login')
+def login(user: User):
+    username = user.username
+    password = user.password
+    return{"message": f"User {username} registered successfully"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0",port=5000, log_level="info" , reload=True)
